f24768b0bfa7a08f8b2edc841c05913299e1f65b
CassioJhones <cassio.bjhones@gmail.com>
2024-05-30 07:51:23 -0300

    formacao do core e regras

---
+++ Fina.Core/Configuration.cs
@@ -0,0 +1,12 @@
+﻿namespace Fina.Core;
+public static class Configuration
+{
+    /// <summary>
+    /// Numero padrao das paginas
+    /// </summary>
+    public const int DefaultPageNumber = 1;
+    /// <summary>
+    /// Tamanho padrao das paginas
+    /// </summary>
+    public const int DefaultPageSize = 25;
+}
---
+++ Fina.Core/Enums/ETransactionType.cs
@@ -0,0 +1,9 @@
+﻿namespace Fina.Core.Enums;
+/// <summary>
+/// Diferentes tipos de transação
+/// </summary>
+public enum ETransactionType
+{
+    Deposit = 1,
+    Withdraw = 2
+}
