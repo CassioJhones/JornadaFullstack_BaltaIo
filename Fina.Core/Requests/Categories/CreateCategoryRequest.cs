3b2e9b69d25e534b8070c9486bdae097ccc350fc
CassioJhones <cassio.bjhones@gmail.com>
2024-05-30 08:10:03 -0300

    metodos para o crud request e response

--- Fina.Core/Requests/Categories/CreateCategoryRequest.cs
+++ Fina.Core/Requests/Categories/CreateCategoryRequest.cs
@@ -1,5 +1,12 @@
-﻿namespace Fina.Core.Requests.Categories;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Fina.Core.Requests.Categories;
 public class CreateCategoryRequest : Request
 {
+    [Required(ErrorMessage ="Titulo Inválido")]
+    [MaxLength(80, ErrorMessage ="O Tiitulo deve conter até 80 caracteres")]
+    public string Title { get; set; } = string.Empty;
 
+    [Required(ErrorMessage ="Descricao Inválida")]
+    public string Description { get; set; } = string.Empty;
 }
