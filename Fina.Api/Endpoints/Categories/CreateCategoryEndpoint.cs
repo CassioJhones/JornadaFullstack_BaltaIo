bd3321e44bdc1c2d691468b4b6113bbf8107a6af
CassioJhones <cassio.bjhones@gmail.com>
2024-06-02 00:00:41 -0300

    create endpoint

--- Fina.Api/Endpoints/Categories/CreateCategoryEndpoint.cs
+++ Fina.Api/Endpoints/Categories/CreateCategoryEndpoint.cs
@@ -1,11 +1,28 @@
 ﻿using Fina.Api.Common.Api;
+using Fina.Core.Handlers;
+using Fina.Core.Models;
+using Fina.Core.Requests.Categories;
+using Fina.Core.Responses;
 
 namespace Fina.Api.Endpoints.Categories;
 
 public class CreateTransactionEndpoint : IEndpoint
 {
     public static void Map(IEndpointRouteBuilder app)
+    => app.MapPost("/", HandleAsync)
+        .WithName("Nome")
+        .WithSummary("Cria nova categoria")
+        .WithDescription("Cria nova categoria")
+        .WithOrder(1)
+        .Produces<Response<Category?>>() ;
+    
+
+    private static async Task<IResult> HandleAsync(ICategoryHandler handler, CreateCategoryRequest request)
     {
-        throw new NotImplementedException();
+        var response = await handler.CreateAsync(request);
+        return response.IsSuccess
+            ? TypedResults.Created($"v1/categories/{response.Data?.Id}", response)
+            : TypedResults.BadRequest(response);
+
     }
 }
