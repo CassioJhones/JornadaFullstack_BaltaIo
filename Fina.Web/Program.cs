db58897604308e968d09f4b35215e27d92428705
CassioJhones <cassio.bjhones@gmail.com>
2024-07-04 17:43:48 -0300

    pagina categorias

--- Fina.Web/Program.cs
+++ Fina.Web/Program.cs
@@ -1,5 +1,7 @@
 using Fina.Core;
+using Fina.Core.Handlers;
 using Fina.Web;
+using Fina.Web.Handlers;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
@@ -11,5 +13,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddMudServices();
 builder.Services.AddHttpClient(WebConfiguration.HttpClienteName,
     opcao => opcao.BaseAddress = new Uri(Configuration.BackendUrl));
+
+builder.Services.AddTransient<ICategoryHandler,CategoryHandler>();
 
 await builder.Build().RunAsync();
